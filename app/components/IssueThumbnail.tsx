99be64811ef5890f513ddcc263815691e42aa696
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-02 21:14:29 -0400

    Implemented edit state and UI

--- app/components/IssueThumbnail.tsx
+++ app/components/IssueThumbnail.tsx
@@ -2,8 +2,9 @@
 import { Issue } from '@prisma/client'
 import { Tooltip } from '@radix-ui/themes'
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import {AiFillCloseCircle, AiFillCheckCircle} from 'react-icons/ai'
+import {MdEdit} from 'react-icons/md'
 
 interface IssueThumbnailProps {
     issue: Issue;
@@ -11,6 +12,8 @@ interface IssueThumbnailProps {
   }
 
   const IssueThumbnail: React.FC<IssueThumbnailProps> = ({ issue, _getData }) => {
+
+    const [isEditing, setIsEditing] = useState(false)
     
     let date = new Date(issue.createdAt)
 
@@ -36,41 +39,64 @@ interface IssueThumbnailProps {
 
     }
 
-  return (
-    <div className='border border-purple-300 rounded-lg p-4 mb-4 w-11/12'>
-        <div className="grid grid-cols-10">
-        <div className="col-span-9">
-            <h2 className='text-purple-300 font-bold'>{issue.title}</h2>
-            <p>{issue.description}</p>
-            <div className="mt-8">
-            <span className='text-purple-300'>Created at: </span>
-            <span>{date.toString()}</span>
+    if(isEditing){
+      return(
+        <div className='border border-purple-300 rounded-lg p-4 mb-4 w-11/12'>
+            <input className='input' type="text" value={issue.title}/>
+        </div>
+      )
+    } else {
+      return (
+        <div className='border border-purple-300 rounded-lg p-4 mb-4 w-11/12'>
+            <div className="grid grid-cols-10">
+            <div className="col-span-9">
+                <h2 className='text-purple-300 font-bold'>{issue.title}</h2>
+                <p>{issue.description}</p>
+                <div className="mt-8">
+                <span className='text-purple-300'>Created at: </span>
+                <span>{date.toDateString()}</span>
+                </div>
+            </div>
+    
+            <div className='float-right'>
+              
+              <div className="float-right">
+                {issue.status == "CLOSED" ? 
+    
+                  <Tooltip content="Reopen Issue">
+                    <button onClick={() => openIssue(issue.id)} onMouseEnter={() => {
+                        displayTooltip("Open Issue")
+                      }}>
+                      <AiFillCheckCircle className='float-right text-2xl text-green-300 hover:text-red-400'/>
+                    </button>
+                  </Tooltip>
+                : 
+                <Tooltip content="Close Issue">
+                  <button onClick={() => closeIssue(issue.id)}>
+                    <AiFillCloseCircle  className='float-right text-2xl text-red-300 hover:text-green-400'/>
+                  </button>
+                </Tooltip>
+                }
+              </div>
+    
+              <div className="clear-right"></div>
+    
+              <Tooltip content="Edit Issue">
+                <button className='float-right'>
+                  <MdEdit className="text-2xl mt-12" onClick={() => {
+                    setIsEditing(true)
+                  }}></MdEdit>
+                </button>
+              </Tooltip>
+    
+              
+                          
+            </div>
             </div>
         </div>
+      )
+    }
 
-        <div className='float-right'>
-          
-          {issue.status == "CLOSED" ? 
-
-            <Tooltip content="Reopen Issue">
-              <button onClick={() => openIssue(issue.id)} onMouseEnter={() => {
-                  displayTooltip("Open Issue")
-                }}>
-                <AiFillCheckCircle className='float-right text-2xl text-green-300 hover:text-red-400'/>
-              </button>
-            </Tooltip>
-          : 
-          <Tooltip content="Close Issue">
-            <button onClick={() => closeIssue(issue.id)}>
-              <AiFillCloseCircle  className='float-right text-2xl text-red-300 hover:text-green-400'/>
-            </button>
-          </Tooltip>
-          }
-                      
-        </div>
-        </div>
-    </div>
-  )
 }
 
 export default IssueThumbnail
